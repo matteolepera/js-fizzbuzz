b371c0ff4d9f8d5eec48c8ca080608ad5f376919
matteolpera <matteo.lepera02@gmail.com>
2025-10-28 14:12:08 +0100

    feat: operazioni scritte a mano.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log("hello world");
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////TRACCIA///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per?
+// Abbiamo visto qualcosa di particolare che possiamo usare?
+
+// Consigli del giorno:
+// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// numeri da 1 a 100 for.
+// multipli:
+//3:
+// numero % 3 === 0; console.log(fizz);
+//5:
+// numero % 5 === 0; console.log(buzz);
+//3&5:
+// numero % 3 === 0 && numero % 5 === 0; console.log(fizzbuzz);
+
+
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+
+}
+
+
